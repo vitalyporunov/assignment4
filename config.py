ce20d3c69d33706caf06649158bddcdb072af4ca
Vitaly Porunov <73434160+vitalyporunov@users.noreply.github.com>
2024-11-12 20:27:23 +0000

    changes for deployment

--- config.py
+++ config.py
@@ -4,5 +4,3 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'supersecretkey'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://assignment4_user:v1pTJeflTALxlefLsFd9onobHNu7ce6f@dpg-cspr2htds78s738s0ngg-a.oregon-postgres.render.com/assignment4'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-**Note: Replace 'postgresql://username:password@localhost/pet_shelter' with your actual PostgreSQL connection URI. Set up environment variables for production with a real secret key and database URI.**
