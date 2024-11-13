fe051cebbee96dc86921b516ede1f240d4dda385
Vitaly Porunov <73434160+vitalyporunov@users.noreply.github.com>
2024-11-13 07:46:29 +0000

    added secret key

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'not-set')
+SECRET_KEY = os.getenv('SECRET_KEY', '1584ffa92daae1b64584c6812331f392f03d780f4e69ced2290b75b94fd23e97')
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://assignment4_user:v1pTJeflTALxlefLsFd9onobHNu7ce6f@dpg-cspr2htds78s738s0ngg-a/assignment4'
