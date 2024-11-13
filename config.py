0eccee89f0be30f5b4fa12188f688c52317c0b6b
Vitaly Porunov <73434160+vitalyporunov@users.noreply.github.com>
2024-11-13 09:17:59 +0000

    changes to config.py file

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'not-set')
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://assignment4_user:v1pTJeflTALxlefLsFd9onobHNu7ce6f@dpg-cspr2htds78s738s0ngg-a/assignment4')
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://assignment4_user:v1pTJeflTALxlefLsFd9onobHNu7ce6f@dpg-cspr2htds78s738s0ngg-a/assignment4")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "not-set")
