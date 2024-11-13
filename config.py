1d8a6b4de6e99420f8b2b3fad0cfe0925125791f
Vitaly Porunov <73434160+vitalyporunov@users.noreply.github.com>
2024-11-13 09:19:30 +0000

    database name changes

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://assignment4_user:v1pTJeflTALxlefLsFd9onobHNu7ce6f@dpg-cspr2htds78s738s0ngg-a/assignment4")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://assignment4_4vxh_user:FbmomyXbQ5PSow0HtrFSZPVD3Bu8FRaT@dpg-csq6cjhu0jms73fo8if0-a.frankfurt-postgres.render.com/assignment4_4vxh")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "not-set")
